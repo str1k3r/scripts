bc51736e0937104546e47b48a357c2106ebcdcb2
Sergey Petrushin <str1k3r@users.noreply.github.com>
2018-03-22 15:46:59 +0300

    Update update-check_mk-agent.ps1

--- update-check_mk-agent.ps1
+++ update-check_mk-agent.ps1
@@ -1,15 +1,16 @@
-$ComputerName = Get-Content 'list.txt'
-$SourceFile = "\\SHARE\check_mk_agent.msi"
-foreach ($Computer in $ComputerName)
+$url = %CMK_AGENT_URL%
+$ComputerName = get-adcomputer -Filter * | Sort-Object
+ 
+foreach ($Computer in $ComputerName.DNSHostName)
 {
     Write-host "Current computer: " $Computer
-    $DestinationFolder = "\\$Computer\C$\Temp\"
-    if (!(Test-Path -path $DestinationFolder))
-    {
-        New-Item $DestinationFolder -Type Directory
-    }
-    Copy-Item -Path $SourceFile -Destination $DestinationFolder -Force
-    Invoke-Command -ComputerName $Computer -ScriptBlock {
+    $DestinationFolder = "\\$Computer\C$\Temp\"
+    if (!(Test-Path -path $DestinationFolder))
+    {
+        New-Item $DestinationFolder -Type Directory
+    }
+    Invoke-WebRequest -Uri "$url" -Out "$DestinationFolder\check_mk_agent.msi"
+    Invoke-Command -ComputerName $Computer -ScriptBlock {
         try {
             Start-Process -FilePath "msiexec" -ArgumentList "/i c:\Temp\check_mk_agent.msi /qn" -Wait
             }
